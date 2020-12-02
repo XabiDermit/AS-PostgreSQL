6cfe26843d90ee1a1b2a0b0cb02aa5e9c53d2507
xabi <xabidermit@gmail.com>
2020-12-02 15:36:21 +0100

    Dockerfile funcionando

--- appcliente/appcliente.py
+++ appcliente/appcliente.py
@@ -16,6 +16,7 @@ try:
 	print(conexion.get_dsn_parameters(), "\n")
 
 	#creamos una tabla, añadimos valores y hacemos una consulta select
+	cursor.execute("DROP TABLE IF EXISTS pruebas_as;")
 	cursor.execute("CREATE TABLE pruebas_as (id varchar(50) PRIMARY KEY, nombre varchar(50) UNIQUE NOT NULL);")
 	cursor.execute("INSERT INTO pruebas_as (id,nombre) VALUES ('1234', 'xabi');")
 	cursor.execute("SELECT * from pruebas_as;")
@@ -24,15 +25,15 @@ try:
 	record = cursor.fetchall()
 	print ("Info:" , record)
 
+	#cerramos la conexion
+	cursor.close()
+	conexion.close()
+	print("Conexion cerrada")
+
 #excepcion si sale mal la conexión
 except (Exception, psycopg2.Error) as error:
 	print("Error en la conexion: ", error)
 
-finally: 
 
-	#si ha abierto la conexion la cerramos
-	if (conexion):
-		cursor.close()
-		conexion.close()
-		print("Conexion cerrada")
+		
 
